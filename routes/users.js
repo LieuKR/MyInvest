192a391bfdfbaa4332eefa7e211cd9f972f27bc4
unknown <korlieu@gmail.com>
2021-03-20 03:42:47 +0900

    Mysql 연결 세팅 및 로그인 페이지 템플릿 작성

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+ // 암호화 모듈 및 암호화 키와 같은 데이터
+const crypto = require('crypto');
+const cryptoconfig  = require('../config/pwcryptset.json');
+const MySqlHandler = require('../serverside_functions/MySqlHandler.js');
+
+
+router.get('/', function(req, res) {
+  res.render('login_form', {pageinfo: 'Login'});
 });
 
 module.exports = router;
