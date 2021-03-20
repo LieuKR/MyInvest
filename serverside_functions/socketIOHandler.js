3ba82659e510cc4cb7dba39018e677a41aa0d8fd
LieuKR <korlieu@gmail.com>
2021-03-21 08:25:16 +0900

    회원가입 제한조건 및 socket를 이용한 유효성 검사 v1

--- serverside_functions/socketIOHandler.js
+++ serverside_functions/socketIOHandler.js
@@ -1,10 +1,45 @@
 // Socket.io 서버사이드 모듈
+const MySqlHandler = require('./MySqlHandler.js');
 
 exports = module.exports = function(io) {
+    // 소켓이 서버에 들어올때의 이벤트들
     io.on('connection', (socket)=> {
-        socket.on("Enter", function(data) {
-            console.log('소켓에서 서버로 반응 옴')
-            io.to(data.socket_id).emit('Enter_reaction', {innertext : '소켓전송 성공'});
-        })
+        // 아이디 유효성 체크
+        socket.on("checkID", function(data) {
+            if (!/^[a-zA-Z0-9]{8,20}$/.test(data.postdata)){
+                io.to(data.socket_id).emit('idcheck', {checkvalue : 2});
+            } else {
+                MySqlHandler.myinvest_mainDB.query(`SELECT EXISTS (SELECT \`id\` FROM \`users\` WHERE \`id\`='${data.postdata}') as success`,
+                (err, rows1) => {
+                    if(rows1[0].success == 1){
+                        io.to(data.socket_id).emit('idcheck', {checkvalue : 1});
+                    } else {
+                        io.to(data.socket_id).emit('idcheck', {checkvalue : 0});
+                    }
+                })
+            };
+        });
+
+        // 이메일 유효성 체크
+        socket.on("checkEmail", function(data) {
+            console.log(data.postdata)
+            let emailRule = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i; //이메일 정규식
+            if (!emailRule.test(data.postdata)){
+                io.to(data.socket_id).emit('mailcheck', {checkvalue : 2});
+            } else {
+                MySqlHandler.myinvest_mainDB.query(`SELECT EXISTS (SELECT \`email\` FROM \`users\` WHERE \`email\`='${data.postdata}') as success`,
+                (err, rows1) => {
+                  if(rows1[0].success == 1){
+                    io.to(data.socket_id).emit('mailcheck', {checkvalue : 1});
+                  } else {
+                    io.to(data.socket_id).emit('mailcheck', {checkvalue : 0});
+                  }
+                })
+            };
+        });
+
+
+
+
     })
 };
