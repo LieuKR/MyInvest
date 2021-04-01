8a63ab8d4249196659ddc5b54f11a895a9bc07a5
LieuKR <korlieu@gmail.com>
2021-04-02 05:23:20 +0900

    가격 갱신 Post 및 서버단 처리 완료

--- routes/int_asset.js
+++ routes/int_asset.js
@@ -39,5 +39,34 @@ router.post('/create_data', function(req, res) {
   })
 });
 
+// 가격 갱신 처리
+router.post('/update_data', function(req, res) {
+  let date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+  function dataupdate_function (status_price) {
+    MySqlHandler.myinvest_personal_DB.query(`INSERT INTO \`${req.session.loginid.id}_asset_recode\` (\`name\`, \`price\`, \`count\`, \`code\`, \`time\`, \`after_count\`) VALUES ('${req.body.name}', '${req.body.price}', 0, '${req.body.code}', '${date}', ${req.body.before_count});`, (err, rows1) => {
+      MySqlHandler.myinvest_personal_DB.query(`UPDATE \`${req.session.loginid.id}_asset_status\` SET \`price\` = '${req.body.price}', \`time\` = '${date}', \`status_price\` = '${status_price}', \`status_count\` = 0
+        WHERE \`name\`= '${req.body.name}'`, (err, rows2) => {
+          if(err) {throw err}
+          else {
+            res.redirect('/int_asset')
+          }
+        })
+    })
+  }
+
+  // 가격이 상승한 경우
+  if(req.body.price - req.body.before_price > 0){
+    let status_price = 1;
+    dataupdate_function (status_price)
+  } else if (req.body.before_price == req.body.price){
+    let status_price = 0;
+    dataupdate_function (status_price)
+  } else {
+    let status_price = -1;
+    dataupdate_function (status_price)
+  }
+
+});
+
 module.exports = router;
 
