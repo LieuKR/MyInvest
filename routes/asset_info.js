7adb682df5c9bef013ff7030a96eabb4e703e402
LieuKR <korlieu@gmail.com>
2021-04-08 08:42:27 +0900

    대시보드 페이지 좌측 테이블에 보유자산, 관심자산 분리되어 표시되게 및 유저정보 드롭다운 수정

--- routes/asset_info.js
+++ routes/asset_info.js
@@ -12,10 +12,12 @@ router.get('/:code', function(req, res) {
   if(req.session.loginid){
     MySqlHandler.myinvest_personal_DB.query(`SELECT * FROM \`${req.session.loginid.id}_asset_status\` WHERE \`code\` = '${req.params.code}'`, (err, rows1) => {
       MySqlHandler.myinvest_personal_DB.query(`SELECT * FROM \`${req.session.loginid.id}_asset_recode\` WHERE \`code\` = '${req.params.code}' ORDER BY \`time\` DESC`, (err, rows2) => {
-        MySqlHandler.myinvest_personal_DB.query(`SELECT \`name\`, \`code\` FROM \`${req.session.loginid.id}_asset_status\` WHERE \`code\` <> '${req.params.code}' ORDER BY \`time\` DESC`, (err, rows3) => {
-          rows1.map(x => time_functions.dateform_time(x));
-          rows2.map(x => time_functions.dateform_time(x));
-          res.render('asset_info', {pageinfo: `자산정보 - ${rows1[0].name}`, pagestatus : '4', loginid : req.session.loginid, asset_status : rows1, asset_recode : rows2, asset_list : rows3});
+        MySqlHandler.myinvest_personal_DB.query(`SELECT \`name\`, \`code\` FROM \`${req.session.loginid.id}_asset_status\` WHERE \`code\` <> '${req.params.code}' AND \`count\` <> 0 ORDER BY \`time\` DESC`, (err, rows3) => {
+          MySqlHandler.myinvest_personal_DB.query(`SELECT \`name\`, \`code\` FROM \`${req.session.loginid.id}_asset_status\` WHERE \`code\` <> '${req.params.code}' AND \`count\` = 0 ORDER BY \`time\` DESC`, (err, rows4) => {
+            rows1.map(x => time_functions.dateform_time(x));
+            rows2.map(x => time_functions.dateform_time(x));
+            res.render('asset_info', {pageinfo: `자산정보 - ${rows1[0].name}`, pagestatus : '4', loginid : req.session.loginid, asset_status : rows1, asset_recode : rows2, asset_own_list : rows3, asset_int_list : rows4});
+          })
         })
       })
     })
